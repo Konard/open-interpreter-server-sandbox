1bd831e548907264b79d5b1563c3b8d54f378457
Konstantin Diachenko <drakonard@gmail.com>
2025-04-22 00:18:19 +0530

    Fix test

--- test-server.sh
+++ test-server.sh
@@ -4,13 +4,21 @@
 echo "Building the Docker image..."
 ./server/build-docker.sh
 
+# Stop and remove any existing container with the same name
+echo "Cleaning up existing Docker container..."
+EXISTING_CONTAINER=$(docker ps -aqf "name=open-interpreter-server")
+if [ -n "$EXISTING_CONTAINER" ]; then
+  docker stop $EXISTING_CONTAINER
+  docker rm $EXISTING_CONTAINER
+fi
+
 # Run the Docker container
 echo "Starting the Docker container..."
 docker run -d --name open-interpreter-server -p 8000:8000 open-interpreter-server
 CONTAINER_ID=$(docker ps -qf "name=open-interpreter-server")
 
 # Wait for the server to start
-sleep 3
+sleep 10
 
 # Test the server with curl
 echo "Testing /chat endpoint with curl..."
@@ -20,8 +28,8 @@ if [[ $CHAT_RESPONSE == *"data:"* ]]; then
   echo "Chat endpoint is working!"
 else
   echo "Chat endpoint test failed!"
-  docker stop $CONTAINER_ID
-  docker rm $CONTAINER_ID
+  # docker stop $CONTAINER_ID
+  # docker rm $CONTAINER_ID
   exit 1
 fi
 
